8a130d38092f6b105dd235d22be308e80be3bf38
mgcvale <miguelcvalealt@gmail.com>
2024-12-05 00:21:17 -0300

    add railway url

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export const CONFIG = {
-    apiBaseUrl: import.meta.env.VITE_API_URL,
+    apiBaseUrl: import.meta.env.VITE_API_URL || "https://sketcho-backend-production.up.railway.app",
 
     getApiUrl(endpoint: string): string {
         console.log("Serving with base url " + this.apiBaseUrl);
